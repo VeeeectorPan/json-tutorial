4840473d248bbc8eb7ac6d34a271d86e8f953189
vecpan <vecpan@whut.edu.cn>
2022-06-14 21:12:21 +0800

    增加json的number检验
    限制number语法到符合json标准

--- tutorial02/leptjson.c
+++ tutorial02/leptjson.c
@@ -2,12 +2,92 @@
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL, strtod() */
 #include <string.h>  /* strlen() */
+#include <errno.h>
+#include <math.h>
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
 
 typedef struct {
     const char* json;
 }lept_context;
+
+static int is_json_number(const char* str, size_t len) {
+    struct st {
+        char lower;
+        char upper;
+        char next;
+    };
+    struct st state0[] = {
+        {'\0', '-', 1},
+        {'\0', '0', 2},
+        {'1', '9', 3},
+        {'\0', '\0', -1},
+    };
+    struct st state1[] = {
+        {'\0', '0', 2},
+        {'1', '9', 3},
+        {'\0', '\0', -1},
+    };
+    struct st state2[] = {
+        {'\0', '.', 4},
+        {'\0', 'e', 5},
+        {'\0', 'E', 5},
+        {'\0', '\0', -1},
+    };
+    struct st state3[] = {
+        {'0', '9', 3},
+        {'\0', '.', 4},
+        {'\0', 'e', 5},
+        {'\0', 'E', 5},
+        {'\0', '\0', -1},
+    };
+    struct st state4[] = {
+        {'0', '9', 6},
+        {'\0', '\0', -1},
+    };
+    struct st state5[] = {
+        {'\0', '+', 7},
+        {'\0', '-', 7},
+        {'0', '9', 8},
+        {'\0', '\0', -1},
+    };
+    struct st state6[] = {
+        {'0', '9', 6},
+        {'\0', 'e', 5},
+        {'\0', 'E', 5},
+        {'\0', '\0', -1},
+    };
+    struct st state7[] = {
+        {'0', '9', 8},
+        {'\0', '\0', -1},
+    };
+    struct st state8[] = {
+        {'0', '9', 8},
+        {'\0', '\0', -1},
+    };
+    struct st* state_table[] = { state0,state1,state2,state3,state4,state5,state6,state7,state8 };
+    int cur = 0;
+    for (int i = 0; i < len; i++) {
+        char input = str[i];
+        int next = -1;
+        for (int j = 0; state_table[cur][j].next != -1; j++) {
+            if (state_table[cur][j].lower && input <= state_table[cur][j].upper && state_table[cur][j].lower <= input) {
+                next = state_table[cur][j].next;
+                break;
+            }
+            else if(state_table[cur][j].upper == input) {
+                next = state_table[cur][j].next;
+                break;
+            }
+            
+        }
+        if (next == -1)
+            return (cur == 2 || cur == 3 || cur == 6 || cur == 8)? LEPT_PARSE_ROOT_NOT_SINGULAR : LEPT_PARSE_INVALID_VALUE;
+        cur = next;
+    }
+
+    return cur == 2 || cur == 3 || cur == 6 || cur == 8 ? LEPT_PARSE_OK : LEPT_PARSE_INVALID_VALUE;
+}
 
 static void lept_parse_whitespace(lept_context* c) {
     const char *p = c->json;
@@ -35,6 +115,14 @@ static int lept_parse_number(lept_context* c, lept_value* v) {
     v->n = strtod(c->json, &end);
     if (c->json == end)
         return LEPT_PARSE_INVALID_VALUE;
+    int ret;
+    if ((ret = is_json_number(c->json, end - c->json)) != LEPT_PARSE_OK) {
+        return ret;
+    }
+    if (errno == ERANGE && (v->n == HUGE_VAL || -v->n == HUGE_VAL)) {
+        errno = 0;
+        return LEPT_PARSE_NUMBER_TOO_BIG;
+    }
     c->json = end;
     v->type = LEPT_NUMBER;
     return LEPT_PARSE_OK;
